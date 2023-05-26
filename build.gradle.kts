fd1c5a6f30475b5a465967f34ad68af4c7d62341
hamburger2506 <hamburger2506@gmail.com>
2023-05-27 00:26:28 +0200

    Added plugin.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 tasks.test {
--- src/main/java/hamburger2506/com/github/futurehitminecraftplugin1/FuturehitMinecraftPlugin1.java
+++ src/main/java/hamburger2506/com/github/futurehitminecraftplugin1/FuturehitMinecraftPlugin1.java
@@ -1,4 +1,22 @@
 package hamburger2506.com.github.futurehitminecraftplugin1;
 
-public class FuturehitMinecraftPlugin1 {
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FuturehitMinecraftPlugin1 extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    }
+
 }
